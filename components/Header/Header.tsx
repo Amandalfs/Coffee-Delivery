4d6f7c4381b664dcf9ada98f3a23a5f266ba7e2e
Amanda <amandalfs57@gmail.com>
2026-01-10 20:39:33 -0300

    feat: create link home and checkout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,22 @@
 import { MapPin } from "lucide-react";
 import Logo from "../Icons/Logo";
 import { MyCartButton } from "../MyCartButton/MyCartButton";
+import Link from "next/link";
 
 export function Header(){
     return <header className="fixed py-8 flex items-center justify-between z-10 bg-white left-0 right-0 mx-40">
-        <Logo className="w-20 h-10"/>
+        
+        <Link href="/">
+            <Logo className="w-20 h-10"/>
+        </Link>
         <div className="flex justify-center items-center gap-2">
             <div className="flex bg-product-purple-light px-2 py-2 rounded-md  gap-2 items-center">
                 <MapPin />
                 <h1 className="text-product-purple-dark text-size-text-S leading-tight font-medium font-base">Jundiaí - Sp</h1>
             </div>
-            <MyCartButton />
+            <Link href={"/checkout"}>
+                <MyCartButton />
+            </Link>
         </div>
     </header>
 }
